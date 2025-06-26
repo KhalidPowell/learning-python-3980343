bb181f7d84b00fbfee81a5c0fcb2754141ac4206
Khalid Powell <khalidpowell13@gmail.com>
2025-06-25 20:38:14 -0400

    "Changes to helloworld_start and variables_start practice files"

--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -11,11 +11,18 @@ mybool = True
 
 # We can display the content of a variable using the print() function
 
+print(mystr)
 
 # Operators are used to perform operations on variables
 
+print(myint + myfloat)
 
 # Logical and comparison operators 
 
+if(mybool == True):
+  print("The boolean was true!")
+
 
 # re-declaring a variable works
+myint = 11
+print(myint)
