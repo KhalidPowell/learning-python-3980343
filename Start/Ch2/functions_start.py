19ce8c7708eb6d42d0155d0b96d8487c371500d5
Khalid Powell <khalidpowell13@gmail.com>
2026-09-10 20:49:37 -0400

    Completed the python tutorial chapter 2

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -1,19 +1,52 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for working with functions
 
-
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+x = 0
+# def hello_func():
+#     print("hello world!")
+#     name = input("What is your name? ")
+#     print("Nice to meet you,", name)
 
+
+# hello_func()
 # function that takes parameters
 
+# def hello_func(greeting):
+#     print("hello world!")
+#     name = input("What is your name? ")
+#     print(greeting, name)
+    
+    
+
+# hello_func("Whats poppin")
+# hello_func("Tryna slide")
 
 # function that returns a value
+# def cube(x):
+#     return x*x*x
 
+# x = cube(3)
 
+# print(x)
 # function with default value for an parameter
 
+# def hello_func(greeting, name=None):
+#     print("hello world!")
+#     if name == None:
+#         name = input("What is your name? ")
+#     print("Nice to meet you,", name)
+    
+
+# hello_func("Hey whats up")
 
 # function with variable number of parameters
+
+# def multi_add(*args):
+#     result = 0
+#     for x in args:
+#         result = result + x
+#     return result
+
+# print(multi_add(3, 56, 6, 23))
+
--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -3,23 +3,35 @@
 
 
 x = 0
+j = 0
+
+days = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"]
 
 # define a while loop
-while x < 5:
-    print(x)
-    x += 1
+# while x < 5:
+#     print(x)
+#     x += 1
     
-answer = input("Should I stop twin?")
-while answer != "yes":
-  print (answer)
-  answer = input("Should I stop now twin?")
+# answer = input("Should I stop twin?")
+# while answer != "yes":
+#   print (answer)
+#   answer = input("Should I stop now twin?")
 
 # define a for loop
-
+# for i in range(10) :
+#   print(i)
 # use a for loop over a collection
+leaders = ["Rocks", "Ace", "Luffy"]
 
+#while j < 3:
+#  print(leaders(j))
 
 # use the break and continue statements
-
+# for d in days:
+#   if d == "thurs":
+#       continue
+#   print(d)
 
 # using the enumerate() function to get an index and an item
+for i,d in enumerate(leaders):
+  print(i, d)
