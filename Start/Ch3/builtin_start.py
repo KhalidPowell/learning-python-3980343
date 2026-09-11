ba95aea3dea394b2f33958d3125cd1e3082a9b20
Khalid Powell <khalidpowell13@gmail.com>
2026-09-11 15:04:45 -0400

    Completed half of chapter 3 exercises

--- Start/Ch3/builtin_start.py
+++ Start/Ch3/builtin_start.py
@@ -6,28 +6,27 @@ mystring = "The quick, brown fox jumped over the lazy dog!"
 my_numbers = [1,3,5,6,9,12,14,17,20,30]
 
 # the len() function calculates the length of a sequence
-print(len(mystring))
+# print(len(mystring))
 
 # the max() and min() functions will find the largest and smallest value in a sequence
-print(max(my_numbers))
-print(min(my_numbers))
+# print(max(my_numbers))
+# print(min(my_numbers))
 
 # the str() function will return a string version of an object
-prefix = "result: "
-result = 5
-prefix = str(prefix)
+# prefix = "result: "
+# result = 5
 
-print(prefix)
+# print(prefix + str(5))
 
 # minor change to test push to github
 
 # range(start, stop, step) will create a range of numbers 
 # You can use ranges along with loops 
-
+print(range(1, 10, 3))
 
 # the print function itself is pretty flexible - you can embed variables directly in it
-greeting = "Hello!"
+greeting = "Hello"
 count = 10
+name = "Luffy"
 
-print(greeting)
-
+print(f"{greeting} {name} you are customer number {count}")
--- Start/Ch3/classes_start.py
+++ Start/Ch3/classes_start.py
@@ -2,3 +2,52 @@
 # Example file for working with classes
 #
 
+class vehicle:
+    def __init__(self, body_style):
+        self.body_style = body_style
+        
+    def drive(self,speed):
+        self.mode = "driving"
+        self.speed = speed
+
+class car(vehicle):
+    def __init__(self, engine_type):
+        super().__init__("Car")
+        self.wheels = 4
+        self.doors = 2
+        self.engine = engine_type
+        
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my", self.engine, "car at", self.speed)
+
+class motorcyle(vehicle):
+    def __init__(self, engine_type, has_side_car):
+        super().__init__("Motorcyle")
+        
+        if(has_side_car):
+            self.wheels = 3
+        else:
+            self.wheels = 2
+            
+        self.doors = 0
+        self.engine = engine_type   
+        
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my", self.engine, "motorcycle at", self.speed)    
+
+
+
+car1 = car("Gas")
+car2 = car("Electric")
+mc1 = motorcyle("Gas", True)
+
+print(mc1.wheels)
+print(car1.engine)
+print(car2.doors)
+
+car1.drive(30)
+car2.drive(55)
+mc1.drive(70)
+
